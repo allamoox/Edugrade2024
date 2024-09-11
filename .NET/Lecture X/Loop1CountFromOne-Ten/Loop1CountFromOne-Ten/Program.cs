39b5946f0cb0f4bb6c419f2b8a9104bb65f92588
x <x@wechall.net>
2024-09-11 21:41:45 +0200

    Uppgift 1 loop Count from 1 - 10

---
+++ .NET/Lecture X/Loop1CountFromOne-Ten/Loop1CountFromOne-Ten/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*
+ 1. LOOP #1
+Skapa ett program som skriver ut talen 0-10 på skärmen. Lös detta med en for-loop.
+ */
+
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine(i);
+}
