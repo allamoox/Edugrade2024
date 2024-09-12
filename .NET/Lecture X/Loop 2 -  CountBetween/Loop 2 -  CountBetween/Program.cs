c5b715737f173396ae7cd5258702fdbb4b365e76
x <x@wechall.net>
2024-09-12 21:11:45 +0200

    Pushing Assignment Loop2

---
+++ .NET/Lecture X/Loop 2 -  CountBetween/Loop 2 -  CountBetween/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* 
+2. LOOP #2
+Skapa ett program där användaren får mata in två tal. Låt sedan programmet skriva ut alla
+tal som finns mellan dessa två tal på skärmen. Lös detta med en for-loop.
+*/
+
+using System.Security.Cryptography;
+
+Console.WriteLine("Please Enter First Number");
+int FirstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Please Enter Second Number");
+int SecondNumber = int.Parse(Console.ReadLine());
+
+int SmaNum = Math.Min(FirstNumber, SecondNumber);
+int BigNum = Math.Max(FirstNumber, SecondNumber);
+Console.WriteLine($"\n Nedan är alla tal som finns mellan {SmaNum} och {BigNum} \n"); 
+SmaNum = SmaNum+1;
+while (SmaNum < BigNum)
+{
+    Console.WriteLine($"{SmaNum}");
+    SmaNum++;
+}
