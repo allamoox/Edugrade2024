c90c5e1673b6900d50ee9bf0905c28c6231ce116
x <x@wechall.net>
2024-09-21 22:58:22 +0200

    Pushing Assignment #6

---
+++ .NET/HomeWork/40 Exercises - Grunderna/6- avUlator/6- avUlator/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ 6. Beräknar medeltalet för 4 tal som användaren matar in.7
+*/
+Console.WriteLine("Enter First Numer");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Second Numer");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Third Numer");
+int ThirdNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Fourth Numer");
+int fourthNumber = int.Parse(Console.ReadLine());
+
+double average = (firstNumber + secondNumber + ThirdNumber + fourthNumber) / 4.0;
+Console.WriteLine(average);
