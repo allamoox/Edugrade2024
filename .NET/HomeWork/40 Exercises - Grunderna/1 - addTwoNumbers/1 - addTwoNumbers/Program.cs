15f3c5de5b092b4074d8743de0a5fc1fd6a932b8
x <x@wechall.net>
2024-09-21 21:50:53 +0200

    Pushing first task

---
+++ .NET/HomeWork/40 Exercises - Grunderna/1 - addTwoNumbers/1 - addTwoNumbers/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ 1. Skriv en konsolapplikation som adderar två heltal och skriver ut resultatet.
+*/
+
+int firstNum = 100;
+int secondNum = 28;
+int result = firstNum + secondNum;
+Console.WriteLine(result);
