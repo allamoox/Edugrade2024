630332e813547bbe2c1a830c6b86eabd8c867c90
x <x@wechall.net>
2024-09-21 22:45:11 +0200

    Pushing assignment number 5

---
+++ .NET/HomeWork/40 Exercises - Grunderna/5- theMultiplier/5- theMultiplier/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+ 5. Tar TRE nummer från användare, multiplicerar dessa och sedan skriver hela 
+     formeln. Ex ”1 x 2 x 3 = 6”
+*/
+Console.WriteLine("Enter First Number");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int secondtNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Third Number");
+int thirdNumber = int.Parse(Console.ReadLine());
+var sum = firstNumber * secondtNumber * thirdNumber;
+Console.WriteLine(sum);
