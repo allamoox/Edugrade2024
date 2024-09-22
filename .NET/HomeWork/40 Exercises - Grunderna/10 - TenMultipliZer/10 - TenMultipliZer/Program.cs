40453829f20f7b71ab067f73f3c1634b0dfebcf8
x <x@wechall.net>
2024-09-22 19:27:22 +0200

    Pushing Assignment  # 10

---
+++ .NET/HomeWork/40 Exercises - Grunderna/10 - TenMultipliZer/10 - TenMultipliZer/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+ 10. Ber användaren mata in ett tal som sedan ska få sin multiplikationstabell 
+       från 0 till 10 utskriven i konsolfönstret.
+*/
+Console.WriteLine("Please Enter a number");
+int userInput = int.Parse(Console.ReadLine());
+Console.WriteLine($"Below is the Multiplication table for {userInput} from 0 to 10");
+for (int i = 0; i <= 10; i++) 
+{
+    Console.WriteLine($"{i} * {userInput} = {i * userInput}");
+}
