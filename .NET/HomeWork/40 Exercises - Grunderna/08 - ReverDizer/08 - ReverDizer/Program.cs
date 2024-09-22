1875ce7ff63865dd55b0e1b3f46d343d063602bf
x <x@wechall.net>
2024-09-22 19:13:55 +0200

    Pushing Assignment 09

---
+++ .NET/HomeWork/40 Exercises - Grunderna/08 - ReverDizer/08 - ReverDizer/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+ 8. Ber användaren om tre bokstäver (en i taget)  och sedan skriver ut dessa i    
+     omvänd ordning från hur användaren matade in dessa.
+*/
+
+Console.WriteLine("You Need To Enter Three Letters, one at a time. Please Enter Your First Letter:");
+char firstLetter = Console.ReadLine()[0];
+Console.WriteLine("Please Enter Your Second Letter:");
+char secondLetter = Console.ReadLine()[0];
+Console.WriteLine("Please Enter Your Third Letter:");
+char thirdLetter = Console.ReadLine()[0];
+Console.WriteLine($"Your Letters Reserved Are {thirdLetter}{secondLetter}{firstLetter}");
---
+++ .NET/HomeWork/40 Exercises - Grunderna/09 - ValuSwaDizer/09 - ValuSwaDizer/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+9. Ber användaren mata in två tal som sparas ner i varsin variabel och sedan 
+     skriver ut båda talen i konsolfönstret. 
+     Sedan ska variablerna byta värde med varandra och sedan skriva ut sina tal 
+     till konsolfönstret igen.
+*/
+
+Console.WriteLine("Please Enter First Number");
+int firstNumber = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Please Enter Second Number");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"First Number is {firstNumber} & Second Number is {secondNumber}");
+int tempVar = firstNumber;
+firstNumber = secondNumber;
+secondNumber = tempVar;
+Console.WriteLine($"First Number is {firstNumber} & Second Number is {secondNumber}");
