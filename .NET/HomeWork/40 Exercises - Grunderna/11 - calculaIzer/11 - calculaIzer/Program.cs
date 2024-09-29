8ebafd4727ae394be30cf6a48c6ad7756238f143
x <x@wechall.net>
2024-09-29 10:43:46 +0200

    Pushing assignment #12

---
+++ .NET/HomeWork/40 Exercises - Grunderna/11 - calculaIzer/11 - calculaIzer/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+ Ber användaren mata in två tal som sedan kommer att adderas, subtraheras, 
+       multipliceras, divideras, restprodukt uträknad, och presenteras i konsolfönstret.
+*/
+
+Console.WriteLine("Please Enter a First Number");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please Enter a Second Number");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"Below is the results of {firstNumber} +,-,* and / by {secondNumber}  /n   {firstNumber} + {secondNumber} = {firstNumber + secondNumber}    {firstNumber} - {secondNumber} = {firstNumber - secondNumber}  {firstNumber} * {secondNumber} = {firstNumber * secondNumber}    {firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
---
+++ .NET/HomeWork/40 Exercises - Grunderna/12- StringTrippler/StringTrippler/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+ . Ber användaren mata in en valfri sträng som sedan skriver ut strängen tre gånger efter  
+       varandra på två olika rader. 
+       Den ena gången utan mellanslag mellan strängarna, den andra helt utan mellanslag 
+       mellan.
+       a. Skriv en lösning där du använder Console.WriteLine().
+       b. Skriv en lösning där du använder $().
+*/
+
+Console.WriteLine("Salam, Please enter a string of your a choice!");
+string userInput = Console.ReadLine();
+Console.WriteLine($"{userInput} {userInput} {userInput}");
+Console.WriteLine($"{userInput}{userInput}{userInput}");
